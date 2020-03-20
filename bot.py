5b926ee67889725f9b60a05bba39131064d00a59
james.loewen <james.loewen@zanbato.com>
2020-03-20 18:18:46 -0400

    Run settings for heroku properly

--- bot.py
+++ bot.py
@@ -3,19 +3,19 @@ import datetime
 import asyncio
 import discord
 
-import settings
+import os
+
+TOKEN = os.environ['TOKEN']
+TIMECHANNEL = os.environ['TIMECHANELL']
 
 client = discord.Client()
-
-def get_formatted_time(timestamp):
-    return f"{timestamp:%H}"
 
 @client.event
 async def on_ready():
     print(f'Logged on as {client.user}')
     while True:
         now = datetime.datetime.now()
-        await client.get_channel(settings.timechannel).edit(name=f"{now:%I-%M-%p-%b-%d} EST") # The channel gets changed here
+        await client.get_channel(TIMECHANNEL).edit(name=f"{now:%I-%M-%p-%b-%d} EST") # The channel gets changed here
         await asyncio.sleep(60)
 
 @client.event
@@ -29,4 +29,4 @@ async def on_message(message):
         now = datetime.datetime.now()
         await message.channel.send(f"It is {now:%I:%M %p} EST on {now:%b %d}")
 
-client.run(settings.TOKEN)
+client.run(TOKEN)
